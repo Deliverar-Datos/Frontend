d460ed7601fd2fd85d2ca459ba059bde1ea0db65
CamilaG-code <cguadagni.sistemas@gmail.com>
2025-06-25 19:11:23 -0300

    Create supervisorRepartos.ts
    rol supervisorRepartos para el guild repartidor

---
+++ frontend-bi/src/config/dashboards/supervisorRepartos.ts
@@ -0,0 +1,35 @@
+export default {
+  email: 'supervisorRepartos@deliverar.com',
+  role: 'supervisorRepartos',
+  layout: 'grid',
+  rows: [
+    {
+      cards: [
+        {
+          //cantidad de pedidos realizados
+          url: '',
+          colSpan: 2,
+        },
+        {
+          //
+          url: '',
+          colSpan: 5,
+        },
+        {
+          //
+          url: '',
+          colSpan: 5,
+        },
+      ],
+    },
+    {
+      cards: [
+        {
+          //Visión General
+          url: '',
+          colSpan: 12,
+        },
+      ],
+    },
+  ]
+};
