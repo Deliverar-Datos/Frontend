4c75c584840a1f13413ea80e8f70535d6e9ee34d
nicopetcoff <nicolaspetcoff@gmail.com>
2025-06-27 12:06:50 -0300

    guild_bi

--- frontend-bi/src/config/dashboards/index.ts
+++ frontend-bi/src/config/dashboards/index.ts
@@ -1,5 +1,6 @@
 import guildmarketplace from './guild-marketplace';
 import ceo from './ceo';
 import backoffice from './backoffice';
+import guild_bi from './guild-bi';
 
-export const dashboardsByUser = [guildmarketplace, ceo, backoffice];
+export const dashboardsByUser = [guildmarketplace, ceo, backoffice, guild_bi];
