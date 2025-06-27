2232615bf46a169703ecbe883755b9fc9a845731
Nico <nicolas.s.fernandez@gmail.com>
2025-06-27 14:19:00 -0300

    Agregar rol supervisor-reparto con configuración de dashboard

--- frontend-bi/src/config/dashboards/index.ts
+++ frontend-bi/src/config/dashboards/index.ts
@@ -1,6 +1,13 @@
-import guildmarketplace from './guild-marketplace';
-import ceo from './ceo';
-import backoffice from './backoffice';
-import guild_bi from './guild-bi';
+import guildmarketplace from "./guild-marketplace";
+import ceo from "./ceo";
+import backoffice from "./backoffice";
+import guild_bi from "./guild-bi";
+import supervisor_reparto from "./supervisor-repartos";
 
-export const dashboardsByUser = [guildmarketplace, ceo, backoffice, guild_bi];
+export const dashboardsByUser = [
+  guildmarketplace,
+  ceo,
+  backoffice,
+  guild_bi,
+  supervisor_reparto
+];
---
+++ frontend-bi/src/config/dashboards/supervisor-repartos.ts
@@ -0,0 +1,15 @@
+export default {
+  role: "supervisor-reparto",
+  rows: [
+    {
+      cards: [
+        {
+          // Dashboard Repartos
+          url:
+            "https://app.powerbi.com/view?r=eyJrIjoiZGZiMmFjZTMtYTY1OC00YWJiLTk1OTItNzAxZWUxZDllODdmIiwidCI6IjdjMTcwZTA1LWQxODEtNDY0OS1hNjZjLTUzYTM1NzUzZGE1NCIsImMiOjR9&pageName=750caf267e92ee1ca66d",
+          colSpan: 12
+        }
+      ]
+    }
+  ]
+};
